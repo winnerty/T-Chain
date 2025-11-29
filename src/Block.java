a0ef56a49b638debf9afdd3de580752bc84f78d1
Winnerty <timofey.n.karpeev@gmail.com>
2025-11-29 01:41:58 +0100

    feat: added Sha256

--- src/Block.java
+++ src/Block.java
@@ -11,6 +11,11 @@ public class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash() {
+        return StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
     }
 
 }
--- src/TChain.java
+++ src/TChain.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
 public class TChain {
+
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+
+    public static void main(String[] args) {
+        blockchain.add(new Block("first block", "0"));
+        blockchain.add(new Block("second block", blockchain.getLast().hash));
+        blockchain.add(new Block("third block", blockchain.getLast().hash));
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+    }
+
 }
