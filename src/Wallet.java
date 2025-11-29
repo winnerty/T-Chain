6d4c0af438fcbbc9edc9a1209903468f9593ee7c
Winnerty <timofey.n.karpeev@gmail.com>
2025-11-29 12:40:37 +0100

    feat: added Wallet class

---
+++ src/Wallet.java
@@ -0,0 +1,25 @@
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+public class Wallet {
+    public PublicKey publicKey;
+    public PrivateKey privateKey;
+
+    public Wallet() {
+        generateKeyPair();
+    }
+
+    public void generateKeyPair() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+            keyGen.initialize(ecSpec, random);
+            KeyPair keyPair = keyGen.generateKeyPair();
+            publicKey = keyPair.getPublic();
+            privateKey = keyPair.getPrivate();
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
